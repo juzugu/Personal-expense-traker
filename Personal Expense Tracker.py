e254b493ae3095b85f05261b51023e53b30202ef
julian zuleta gutierrez <julian65zuleta@hotmail.com>
2025-12-18 19:04:36 -0500

    Create Personal Expense Tracker.py

---
+++ Personal Expense Tracker.py
@@ -0,0 +1,11 @@
+my_expenses = [
+    {"product":"milk",
+     "date":17/12/2026,
+     "price":4500},
+    {"product":"bread",
+     "date":18/12/2026,
+     "price":2000},
+    {"product":"eggs",
+     "date":19/12/2026,
+     "price":1500}
+]
