3824238e50caeb80e738f4b400c7b7ac1dfad77d
julian zuleta gutierrez <julian65zuleta@hotmail.com>
2025-12-20 17:51:02 -0500

    Update Personal Expense Tracker.py

--- Personal Expense Tracker.py
+++ Personal Expense Tracker.py
@@ -1,11 +1,28 @@
-my_expenses = [
-    {"product":"milk",
-     "date":17/12/2026,
-     "price":4500},
-    {"product":"bread",
-     "date":18/12/2026,
-     "price":2000},
-    {"product":"eggs",
-     "date":19/12/2026,
-     "price":1500}
-]
+import json
+def save(data_to_save):
+    with open("saving_data.json", "w") as f:
+        json.dump({"saving_data.json": data_to_save}, f)
+def load():
+    with open("saving_data.json", "r") as f:
+        return json.load(f).get("saving_data.json", [])
+my_expenses = []
+
+def input_for_expenses ():
+    product_bought = input("Enter the product you bought ")
+    quantity = int(input("Enter the quantity bought if "))
+    price = int(input("Enter the item price "))
+    return product_bought, quantity, price
+while True:
+    product_bought, quantity, price = input_for_expenses()
+    my_expenses.append({"product": product_bought, "quantity": quantity, "price": price})
+    want_to_continue = input("Do you want to continue? yes/no ")
+    if want_to_continue.strip().lower() == "no":
+        break
+print(my_expenses)
+total_expense = 0
+for expense in my_expenses:
+    expense_vault = expense["quantity"] * expense["price"]
+    total_expense = total_expense + expense_vault
+    print(f"the expense for {expense['product']} is {expense_vault}")
+print(f"total expense is {total_expense}")
+save(my_expenses)
