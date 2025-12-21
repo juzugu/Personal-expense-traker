71cf368a94bc5e65e804131896844c6440305455
julian zuleta gutierrez <julian65zuleta@hotmail.com>
2025-12-20 19:08:57 -0500

    Update Personal Expense Tracker.py
    new delete funtion

--- Personal Expense Tracker.py
+++ Personal Expense Tracker.py
@@ -9,6 +9,8 @@ def load():
             return json.load(f).get("saving_data.json", [])
     except FileNotFoundError:
             return []
+    
+budget = int(input("Enter your budget for this "))
 
 my_expenses = load()
 
@@ -23,11 +25,23 @@ while True:
     want_to_continue = input("Do you want to continue? yes/no ")
     if want_to_continue.strip().lower() == "no":
         break
+delete = input("Do you want to delete any item? yes/no ")
+if delete.strip().lower() == "yes":
+    index_to_delete = int(input("Enter the number of the item to delete "))
+    if 0 <= index_to_delete and index_to_delete < len(my_expenses):
+        my_expenses.pop(index_to_delete)
+    else:
+        print("Invalid number")
+
 print(my_expenses)
 total_expense = 0
-for expense in my_expenses:
+for index, expense in enumerate(my_expenses):
     expense_vault = expense["quantity"] * expense["price"]
     total_expense = total_expense + expense_vault
-    print(f"the expense for {expense['product']} is {expense_vault}")
+    print(f"the expense for {index} {expense['product']} is {expense_vault}")
+if total_expense > budget:
+    print(f"You have exceeded your budget of {budget} for {total_expense - budget}")
+else:
+    print(f"You are within the budget. You have {budget - total_expense} remaining")
 print(f"total expense is {total_expense}")
 save(my_expenses)
